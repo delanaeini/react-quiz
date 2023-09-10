40f3832a547e55ce0d699f60e496752849d8f579
Dela Naeini <delaram.naeini@gmail.com>
2023-09-10 13:58:22 -0700

    timer, readme update

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # react-quiz
+
+A React app for testing basic React knowledge
+
+## Description
+
+This app contains a timed quiz to test the user's knowledge of React concepts. \n\nEach question has a different number of points, and as the user answers each question correctly, the points add up. When the test is finished(or when the time runs out), the user can see their points, and the percentage of the questions they answered correctly.\n\nThe user can also see their highscore which is reserved across attempts.
+
+## Installation
+
+- Install npm if you don't see a version number from the following command on the terminal.
+
+```
+npm -v
+```
+
+- Install node modules
+
+```
+npm install
+```
+
+- Run the questions data
+
+```
+npm run server
+```
+
+- On a different terminal tab, run the app
+
+```
+npm start
+```
+
+## Credits
+
+This project was created by the amazing Udemy instructor, Jonas Schmedtmann, for his wonderful course of Ultimate React Tutorial
+https://www.udemy.com/course/the-ultimate-react-course/
+https://github.com/jonasschmedtmann/
--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useReducer } from "react";
+
 import Header from "./Header";
 import Main from "./Main";
 import Loader from "./Loader";
@@ -8,6 +9,10 @@ import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
 import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
+
+const SECS_PER_QUESTION = 30;
 
 const initialState = {
   questions: [],
@@ -18,6 +23,7 @@ const initialState = {
   answer: null,
   points: 0,
   highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -37,6 +43,7 @@ function reducer(state, action) {
       return {
         ...state,
         status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
       };
     case "newAnswer":
       const question = state.questions.at(state.index); //Get the question itself from the index
@@ -64,6 +71,16 @@ function reducer(state, action) {
         status: "ready",
         highscore: state.highscore,
       };
+
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+
     default:
       throw new Error("Action unknown");
   }
@@ -71,8 +88,10 @@ function reducer(state, action) {
 
 export default function App() {
   //destructuring state
-  const [{ questions, status, index, answer, points, highscore }, dispatch] =
-    useReducer(reducer, initialState);
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce(
@@ -111,12 +130,15 @@ export default function App() {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton
-              dispatch={dispatch}
-              answer={answer}
-              index={index}
-              numQuestions={numQuestions}
-            />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                index={index}
+                numQuestions={numQuestions}
+              />
+            </Footer>
           </>
         )}
         {status === "finished" && (
